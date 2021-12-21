2adf8dfa5bd1a52d62b6038fdbf77f9ea7969dd9
andreaceresetti <andrea1.ceresetti@mail.polimi.it>
2021-12-21 14:20:52 +0100

    Commit

--- DB2ProjectWEB/src/main/java/it/polimi/db2/controllers/CheckLogin.java
+++ DB2ProjectWEB/src/main/java/it/polimi/db2/controllers/CheckLogin.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.json.JSONObject;
+import org.jose4j.json.internal.json_simple.JSONObject;
 
 //import org.jose4j.json.internal.json_simple.JSONObject;
 
